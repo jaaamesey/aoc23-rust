83ba0a21f5af83eb1a9acb211458f78fe81f3ce8
James Karlsson <jameskarlsson1@gmail.com>
2023-12-03 22:19:20 +1100

    Day3 full

--- src/day03/mod.rs
+++ src/day03/mod.rs
@@ -1,9 +1,15 @@
+use std::collections::HashMap;
+
 #[derive(Debug)]
 struct NumberAtPos { number: i32, row: usize, start_column: usize }
 
 const ADJACENCY_OFFSETS: [(i32, i32); 8] = [(0, 1), (0, -1), (1, 0), (-1, 0), (1, 1), (-1, 1), (1, -1), (-1, -1)];
 
 pub fn part1() {
+    fn is_part_symbol(c: char) -> bool {
+        !c.is_numeric() && c != '.'
+    }
+    
     let rows: Vec<Vec<char>> = include_str!("./real_input.txt")
         .split('\n').map(|str| str.chars().collect()).collect();
     
@@ -34,15 +40,15 @@ pub fn part1() {
         for digit_offset in 0..num_digits {
             let x = num.start_column + digit_offset;
             
-            for (adj_offset_y, adj_offset_x) in ADJACENCY_OFFSETS {
-                let row = rows.get(y.checked_add_signed(adj_offset_y.try_into().unwrap()).unwrap_or(0));
-                
-                if row.is_none() {
-                    continue;
+            for (check_y, check_x) in ADJACENCY_OFFSETS.iter().filter_map(|(dir_y, dir_x)| {
+                let check_y: isize = (y as isize) + (*dir_y as isize);
+                let check_x: isize = (x as isize) + (*dir_x as isize);
+                if check_x < 0 || check_y < 0 || check_x >= rows[0].len().try_into().unwrap() || check_y >= rows.len().try_into().unwrap() {
+                    return None;
                 }
-
-                let c = row.unwrap().get(x.checked_add_signed(adj_offset_x.try_into().unwrap()).unwrap_or(0)); 
-                if c.is_some() && is_part_symbol(*c.unwrap()) {
+                return Some((check_y as usize, check_x as usize));
+            }) {
+                if is_part_symbol(rows[check_y][check_x]) {
                     return true;
                 }
             }
@@ -54,6 +60,64 @@ pub fn part1() {
     dbg!(output);
 }
 
-fn is_part_symbol(c: char) -> bool {
-    !c.is_numeric() && c != '.'
+pub fn part2() {
+    let rows: Vec<Vec<char>> = include_str!("./real_input.txt")
+        .split('\n').map(|str| str.chars().collect()).collect();
+    
+    let mut numbers: Vec<NumberAtPos> = Vec::new();
+    for (y, row) in rows.iter().enumerate() {
+        let mut curr_str = "".to_string();
+        for (x, column) in row.iter().enumerate() {
+            if column.is_numeric() {
+                curr_str.push(*column);
+            }
+
+            if curr_str.len() > 0 && (!column.is_numeric() || x == row.len() - 1) {
+                numbers.push(NumberAtPos { 
+                    number: curr_str.parse::<i32>().unwrap(),
+                    row: y,
+                    start_column: x - curr_str.len(),
+                });
+                curr_str.clear();
+            }
+        }
+    }
+
+    let mut gears: HashMap<String, Vec<i32>> = HashMap::new();
+
+    numbers.iter().for_each(|num| {
+        let num_digits = num.number.to_string().len(); 
+
+        let y = num.row;
+
+        for digit_offset in 0..num_digits {
+            let x = num.start_column + digit_offset;
+            
+            for (check_y, check_x) in ADJACENCY_OFFSETS.iter().filter_map(|(dir_y, dir_x)| {
+                let check_y: isize = (y as isize) + (*dir_y as isize);
+                let check_x: isize = (x as isize) + (*dir_x as isize);
+                if check_x < 0 || check_y < 0 || check_x >= rows[0].len().try_into().unwrap() || check_y >= rows.len().try_into().unwrap() {
+                    return None;
+                }
+                return Some((check_y as usize, check_x as usize));
+            }) {
+                let potential_symbol = rows[check_y][check_x];
+                if potential_symbol == '*' {
+                    let key = format!("{},{}", check_x, check_y);
+                    let surrounding_numbers = gears.entry(key).or_insert_with(|| Vec::new());
+                    surrounding_numbers.push(num.number);
+                    return;
+                }
+            }
+        }
+    });
+
+    let output = gears.iter().fold(0, |acc, (_, surrounding_numbers)| {
+        if surrounding_numbers.len() != 2 {
+            return acc;
+        }
+        return acc + (surrounding_numbers[0] * surrounding_numbers[1]);
+    });
+
+    dbg!(output);
 }
--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,6 @@ mod day03;
 
 fn main() {
     day03::part1();
+    day03::part2();
 }
 
