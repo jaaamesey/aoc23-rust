fd06b5ab8b5bdec83ba7e7c5885262e62e2c12b7
James Karlsson <jameskarlsson1@gmail.com>
2023-12-04 20:46:37 +1100

    Day 4 finnito

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -1,7 +1,5 @@
-use std::collections::HashSet;
-
 pub fn part1() {
-    let output = include_str!("./real_input.txt")
+    let output = include_str!("./test_input.txt")
         .lines()
         .fold(0, |acc, line| {
             let (winning_numbers, given_numbers) = {
@@ -31,7 +29,47 @@ pub fn part1() {
     dbg!(output);
 }
 
-pub fn part1_andrew() {
+pub fn part2() {
+    let mut total_scratchcards = 0;
+
+    let lines = include_str!("./real_input.txt").lines();
+
+    let mut num_cards_left = vec![1; lines.clone().count()];
+
+    lines.enumerate().for_each(|(i, line)| {
+        let (winning_numbers, given_numbers) = {
+            let mut winning_and_given = line
+                .split(": ")
+                .nth(1)
+                .expect("Missing half after card numbers")
+                .split(" | ")
+                .map(|str| str.split_whitespace());
+            (
+                winning_and_given.next().expect("Missing winning numbers"),
+                winning_and_given.next().expect("Missing given numbers"),
+            )
+        };
+        let num_matches = given_numbers
+            .filter(|num| winning_numbers.clone().any(|n| n == *num))
+            .count();
+
+        while num_cards_left[i] > 0 {
+            total_scratchcards += 1;
+
+            let mut cards_to_copy = num_matches;
+
+            num_cards_left[i] -= 1;
+
+            while cards_to_copy > 0 {
+                num_cards_left[i + cards_to_copy] += 1;
+                cards_to_copy -= 1;
+            }
+        }
+    });
+    dbg!(total_scratchcards);
+}
+
+pub fn _part1_andrew() {
     let output = include_str!("./real_input.txt")
         .lines()
         .fold(0, |points, line| {
@@ -61,5 +99,3 @@ pub fn part1_andrew() {
         });
     dbg!(output);
 }
-
-pub fn part2() {}
