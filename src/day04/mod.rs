a51e343670ca717267a7e2d78959e507d069509f
James Karlsson <jameskarlsson1@gmail.com>
2023-12-04 19:40:21 +1100

    Improved perf

--- src/day04/mod.rs
+++ src/day04/mod.rs
@@ -10,20 +10,14 @@ pub fn part1() {
                     .nth(1)
                     .expect("Missing half after card numbers")
                     .split(" | ")
-                    .map(|str| {
-                        str.split_whitespace()
-                            .map(|item| item.parse::<i32>().expect("Could not parse as int"))
-                    });
+                    .map(|str| str.split_whitespace());
                 (
-                    winning_and_given
-                        .next()
-                        .expect("Missing winning numbers")
-                        .collect::<HashSet<_>>(),
+                    winning_and_given.next().expect("Missing winning numbers"),
                     winning_and_given.next().expect("Missing given numbers"),
                 )
             };
             let line_points = given_numbers.fold(0, |acc, num| {
-                if winning_numbers.contains(&num) {
+                if winning_numbers.clone().any(|n| n == num) {
                     if acc == 0 {
                         return 1;
                     }
@@ -34,6 +28,37 @@ pub fn part1() {
             acc + line_points
         });
 
+    dbg!(output);
+}
+
+pub fn part1_andrew() {
+    let output = include_str!("./real_input.txt")
+        .lines()
+        .fold(0, |points, line| {
+            let [winners, havers] = line
+                .split(": ")
+                .nth(1)
+                .unwrap()
+                .split(" | ")
+                .collect::<Vec<_>>()[..]
+            else {
+                todo!()
+            };
+            let win_vec: Vec<_> = winners.split_whitespace().collect();
+            let have_vec: Vec<_> = havers.split_whitespace().collect();
+            let mut line_points = 0;
+
+            for val in have_vec {
+                if win_vec.contains(&val) {
+                    if line_points == 0 {
+                        line_points = 1;
+                    } else {
+                        line_points *= 2;
+                    }
+                }
+            }
+            return points + line_points;
+        });
     dbg!(output);
 }
 
