40dd6da7f8c2074bf1a75e3ef6b01fbba109b89a
James Karlsson <jameskarlsson1@gmail.com>
2023-12-06 18:29:32 +1100

    Solved 5-2 with brute force (~100 seconds)

--- src/day05/mod.rs
+++ src/day05/mod.rs
@@ -1,4 +1,3 @@
-use std::num;
 use std::ops::Range;
 
 #[derive(Debug)]
@@ -63,11 +62,12 @@ pub fn part1() {
     dbg!(output.unwrap());
 }
 
+// Buckle up, this took about 106 seconds on a release build
 pub fn part2() {
     let mut line_iter = INPUT.lines().filter(|line| !line.is_empty());
 
     let seeds = {
-        let mut seeds = line_iter
+        let seeds = line_iter
             .next()
             .unwrap()
             .replace("seeds: ", "")
@@ -75,26 +75,16 @@ pub fn part2() {
             .map(|str| str.parse::<i64>().unwrap())
             .collect::<Vec<_>>();
 
-        let gcd = seeds
-            .iter()
-            .map(|&n| n)
-            .reduce(|acc, curr| gcd(acc, curr))
-            .unwrap();
-        dbg!(gcd);
-        seeds.iter().map(|n| n / gcd).collect::<Vec<_>>()
-
-        // let mut seeds_iter = seeds.iter();
-        // let mut curr_seeds = seeds.iter().next();
-        // let mut ranges = Vec::<Range<i64>>::new();
-        // while curr_seeds.is_some() {
-        //     let start = *curr_seeds.unwrap();
-        //     ranges.push(start..(start + *seeds_iter.next().unwrap()));
-        //     curr_seeds = seeds_iter.next();
-        // }
-        // ranges
+        let mut seeds_iter = seeds.iter();
+        let mut curr_seeds = seeds_iter.next();
+        let mut ranges = Vec::<Range<i64>>::new();
+        while curr_seeds.is_some() {
+            let start = *curr_seeds.unwrap();
+            ranges.push(start..(start + seeds_iter.next().unwrap()));
+            curr_seeds = seeds_iter.next();
+        }
+        ranges
     };
-
-    dbg!(seeds);
 
     let mut curr_line = line_iter.next();
 
@@ -118,21 +108,25 @@ pub fn part2() {
 
         curr_line = line_iter.next();
     }
-}
 
-fn gcd(mut a: i64, mut b: i64) -> i64 {
-    if a == b {
-        return a;
-    }
-    if b > a {
-        let temp = a;
-        a = b;
-        b = temp;
-    }
-    while b > 0 {
-        let temp = a;
-        a = b;
-        b = temp % b;
-    }
-    return a;
+    let output = seeds
+        .iter()
+        .map(|r| r.clone().into_iter())
+        .flatten()
+        .map(|seed| {
+            maps.iter().fold(seed, |acc, map| {
+                let applicable_range = map.iter().find(|range| {
+                    acc >= range.source_start && acc <= range.source_start + range.range_length
+                });
+                if applicable_range.is_some() {
+                    let range = applicable_range.unwrap();
+                    let offset = range.destination_start - range.source_start;
+                    return acc + offset;
+                }
+                return acc;
+            })
+        })
+        .min();
+
+    dbg!(output.unwrap());
 }
