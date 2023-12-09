2e5cf6f5b48994a3de80bd13bb42c6eebf16f30d
James Karlsson <jameskarlsson1@gmail.com>
2023-12-09 20:27:34 +1100

    Part 2 done

--- src/day09/mod.rs
+++ src/day09/mod.rs
@@ -1,6 +1,17 @@
+use std::collections::VecDeque;
+
 const INPUT: &str = include_str!("./real_input.txt");
 
 pub fn part1() {
+    fn get_diffs(input: &Vec<i64>) -> Vec<i64> {
+        let mut output_diffs = Vec::new();
+        for i in 1..input.len() {
+            let diff = input[i] - input[i - 1].clone();
+            output_diffs.push(diff);
+        }
+        return output_diffs;
+    }
+    
     let output = INPUT.lines().fold(0, |acc, line| {
         let mut stack = {
             let initial_numbers = line.split_whitespace().map(|s| s.parse::<i64>().unwrap()).collect::<Vec<_>>();
@@ -29,18 +40,41 @@ pub fn part1() {
     dbg!(output);
 }
 
-pub fn part2() {}
-
-fn get_diffs(input: &Vec<i64>) -> Vec<i64> {
-    let mut output_diffs = Vec::new();
-    for i in 1..input.len() {
-        let diff = input[i] - input[i - 1].clone();
-        output_diffs.push(diff);
+pub fn part2() {
+    fn get_diffs(input: &VecDeque<i64>) -> VecDeque<i64> {
+        let mut output_diffs = VecDeque::new();
+        for i in 1..input.len() {
+            let diff = input[i] - input[i - 1].clone();
+            output_diffs.push_back(diff);
+        }
+        return output_diffs;
     }
-    return output_diffs;
+    
+    let output = INPUT.lines().fold(0, |acc, line| {
+        let mut stack = {
+            let initial_numbers = line.split_whitespace().map(|s| s.parse::<i64>().unwrap()).collect::<VecDeque<_>>();
+            let mut stack = Vec::<VecDeque<i64>>::new();
+            stack.push(initial_numbers);
+            stack
+        };
+
+        while !stack.last().unwrap().iter().all(|&n| n == 0) {
+            stack.push(get_diffs(&stack.last().unwrap()));
+        }
+
+        (stack.last_mut().unwrap()).push_front(0);
+
+        while stack.len() > 1 {
+            let &first_item = stack.pop().unwrap().front().unwrap();
+            let next_line_up = stack.last_mut().unwrap();
+            next_line_up.push_front(next_line_up.front().unwrap() - first_item);
+        }
+
+        let &next_in_sequence = stack.last().unwrap().front().unwrap();
+
+        return acc + next_in_sequence;
+    });
+
+    dbg!(output);
 }
 
-// fn get_diff(input: &Vec<i64>) -> i64 {
-//     let mut iter = input.iter();
-//     return iter.last().unwrap() - iter.next_back().unwrap();
-// }
