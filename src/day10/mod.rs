9074be72547e44b2926d879d2c79c568d1a55e65
James Karlsson <jameskarlsson1@gmail.com>
2023-12-11 12:04:24 +1100

    Working on test input

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -22,59 +22,45 @@ pub fn part1() {
 
     fn get_direction(char: char, last_dir: (i8, i8)) -> Option<(i8, i8)> {
         match char {
-            '-' if last_dir.1 != -1 => Some((0, 1)),
-            '-' if last_dir.1 != 1 => Some((0, -1)),
+            '-' if last_dir.1 == -1 => Some((0, -1)),
+            '-' if last_dir.1 == 1 => Some((0, 1)),
 
-            '|' if last_dir.0 != -1 => Some((1, 0)),
-            '|' if last_dir.0 != 1 => Some((-1, 0)),
+            '|' if last_dir.0 == -1 => Some((-1, 0)),
+            '|' if last_dir.0 == 1 => Some((1, 0)),
 
-            'F' if last_dir.1 != -1 => Some((0, 1)),
-            'F' if last_dir.0 != -1 => Some((1, 0)),
+            'F' if last_dir.1 == -1 => Some((1, 0)),
+            'F' if last_dir.0 == -1 => Some((0, 1)),
 
-            '7' if last_dir.1 != 1 => Some((0, -1)),
-            '7' if last_dir.0 != -1 => Some((1, 0)),
+            '7' if last_dir.1 == 1 => Some((1, 0)),
+            '7' if last_dir.0 == -1 => Some((0, -1)),
 
-            'L' if last_dir.1 != -1 => Some((0, 1)),
-            'L' if last_dir.0 != 1 => Some((-1, 0)),
+            'L' if last_dir.1 == -1 => Some((-1, 0)),
+            'L' if last_dir.0 == 1 => Some((0, 1)),
 
-            'J' if last_dir.1 != 1 => Some((0, -1)),
-            'J' if last_dir.0 != 1 => Some((-1, 0)),
+            'J' if last_dir.1 == 1 => Some((-1, 0)),
+            'J' if last_dir.0 == 1 => Some((0, -1)),
             _ => None,
         }
     }
 
-    const ALL_DIRECTIONS: [(i8, i8); 4] = [(0, 1), (0, -1), (1, 0), (-1, 0)];
-
-    let mut cursor = starting_pos;
-    let mut last_dir = (0, 0);
-    let mut step_count = 0;
+    let mut cursor = (starting_pos.0, starting_pos.1 + 1);
+    let mut last_dir = (0, 1); // HARDCODED
+    let mut step_count = 1;
 
     loop {
-        let tuple: Option<((i8, i8), (i32, i32))> = ALL_DIRECTIONS.iter().find_map(|&dir| {
-            if dir == (-last_dir.0, -last_dir.1) {
-                return None;
-            }
-            let pos = (cursor.0 + (dir.0 as i32), cursor.1 + (dir.1 as i32));
-            if pos.0 < 0 || pos.1 < 0 {
-                return None;
-            }
-            let char = grid.get(pos.0 as usize, pos.1 as usize);
+        let char = grid[(cursor.0 as usize, cursor.1 as usize)];
+        let dir_to_move = get_direction(char, last_dir).unwrap();
 
-            if char.is_some() {
-                let dir = get_direction(*char.unwrap(), last_dir);
-                if dir.is_some() {
-                    dbg!(char);
-                    return Some((dir.unwrap(), pos));
-                }
-            }
-            return None;
-        });
+        dbg!(dir_to_move);
 
-        let (next_dir, pos) = tuple.unwrap();
-        dbg!(pos);
-        cursor = pos;
+        cursor = (
+            cursor.0 + (dir_to_move.0 as i32),
+            cursor.1 + (dir_to_move.1 as i32),
+        );
 
-        last_dir = next_dir;
+        dbg!(cursor);
+
+        last_dir = dir_to_move;
 
         step_count += 1;
 
@@ -83,7 +69,7 @@ pub fn part1() {
         }
     }
 
-    dbg!(step_count);
+    dbg!(step_count / 2);
 }
 
 pub fn part2() {}
