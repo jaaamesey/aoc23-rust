9dfbc0d458dab5d73bec779e8b3576902a855a9c
James Karlsson <jameskarlsson1@gmail.com>
2024-01-19 22:21:59 +1100

    Setup day 12

---
+++ src/day12/mod.rs
@@ -0,0 +1,5 @@
+const INPUT: &str = include_str!("./test_input.txt");
+
+pub fn part1() {}
+
+pub fn part2() {}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
-mod day10;
+mod day12;
 
 fn main() {
     let start = std::time::Instant::now();
-    day10::part1();
-    day10::part2();
+    day12::part1();
+    day12::part2();
     println!("Finished in {:?}", start.elapsed());
 }
